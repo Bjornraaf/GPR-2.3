8cc2e6ad9cee543592fa42b9ce0e8a2d79505113
Bjornraaf <daysforyou1120@gmail.com>
2021-02-08 13:28:32 +0100

    Coroutines & Lineair Interpolation 1.2
    Tweede opdracht form

---
+++ GPR CLI/Assets/1/Scipts/GPR12.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPR12 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Fade");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Fade()
+    {
+        Debug.Log("Ik start nu de coroutine");
+        yield return new WaitForSeconds(.5f);
+
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        {
+            Debug.Log("Coroutine update");
+            yield return new WaitForSeconds(.5f);
+        }
+
+        Debug.Log("Coroutine einde");
+        yield return new WaitForSeconds(.5f);
+    }
+}
